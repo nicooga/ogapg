5df1730343f1602790ec5a5bd1d699b889f8697e
Nicolas Oga <2112.oga@gmail.com>
2012-10-09 19:21:28 -0300

    Brand new app, postgresql db set.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ogapg::Application.config.secret_token = 'd13febe52303a23347b1c573b1efbdf41e548152e6a47f2ba5b27cd0ae3bc3c8173c4460c81dd3f945ebf63d2b111c22f55b86cb0d8d9efd947e4c24eb9bbb32'
